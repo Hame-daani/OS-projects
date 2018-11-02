dd8515a74722b598c140a9bad70d1c4143b6f199
Sadegh Hmp <sad3gh.hmp@gmail.com>
2018-11-02 15:33:35 +0330

    memory created and tested

--- Project_1/Knapsack.cpp
+++ Project_1/Knapsack.cpp
@@ -7,11 +7,53 @@
 
 using namespace std;
 
+struct Memory
+{
+    pid_t *IDs;
+};
+
+#define MEMORY_SIZE sizeof(Memory)
+#define MEMORY_NAME "shm"
+
 int main()
 {
     // get number of proccesors
     int numCPU = (int)sysconf(_SC_NPROCESSORS_ONLN);
     cout << "Number of Processors is " + to_string(numCPU) << endl;
-    
+
+    //create shared memory
+    int shm_fd;
+    if ((shm_fd = shm_open(MEMORY_NAME, O_CREAT | O_RDWR, 0666)) == -1)
+    {
+        cout << "error in shm open" << endl;
+        return 0;
+    }
+    if ((ftruncate(shm_fd, MEMORY_SIZE)) == -1)
+    {
+        cout << "error in ftruncate" << endl;
+        return 0;
+    }
+    Memory *shm = (Memory *)mmap(0, MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    // test memory
+    shm->IDs = new pid_t[numCPU];
+    for (int i = 0; i < numCPU; i++)
+        shm->IDs[i] = getpid() + i;
+    pid_t pid;
+    pid = fork();
+    if (pid == 0)
+    {
+        for (int i = 0; i < numCPU; i++)
+            cout << shm->IDs[i] << endl;
+        cout << "child done" << endl;
+        return 0;
+    }
+    if (pid > 0)
+    {
+        wait(NULL);
+        cout << "parent done" << endl;
+        return 0;
+    }
+
     return 0;
 }
