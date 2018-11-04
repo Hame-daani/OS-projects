1679468d214469e9c6554cb750389b488b4efba0
Sadegh Hmp <sad3gh.hmp@gmail.com>
2018-11-04 21:36:21 +0330

    memory works. child recognize themself.

--- Project_1/Knapsack.cpp
+++ Project_1/Knapsack.cpp
@@ -1,49 +1,51 @@
-#include <sys/wait.h>
 #include <iostream>
 #include <unistd.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
+#include <sys/mman.h> /* for shm_open */
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>    /* For O_* constants */
+#include <signal.h>
+#include <sys/wait.h>
 
 using namespace std;
 
 int main()
 {
-    const int SIZE = 4096;
-    const char *name = "OS";
-    const char *msg_0 = "Hello";
-    const char *msg_1 = "World!";
+    int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
+    const char *ids_shm_name = "ids_shm";
+    const int ids_shm_size = sizeof(pid_t) * numCPU + 1;
 
     int fd;
-    char *ptr;
-    fd = shm_open(name, O_CREAT | O_RDWR, 0666);
-    ftruncate(fd, SIZE);
-    ptr = (char *)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    sprintf(ptr, "%s", msg_0);
-    ptr += strlen(msg_0);
-    sprintf(ptr, "%s", msg_1);
-    ptr += strlen(msg_1);
+    fd = shm_open(ids_shm_name, O_CREAT | O_RDWR, 0666);
+    ftruncate(fd, ids_shm_size);
+    pid_t *ids_shm = (pid_t *)mmap(0, ids_shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
-    pid_t pid;
-    pid = fork();
+    pid_t pid, id;
+    id = getpid();
+    *ids_shm = id;
+    ids_shm += sizeof(id);
+    for (int i = 0; i < numCPU; i++)
+    {
+        pid = fork();
+        if (pid == 0)
+            break;
+        *ids_shm = pid;
+        ids_shm += sizeof(pid);
+    }
     if (pid == 0)
     {
-        cout << "child start." << endl;
         execl("child.o", "child.o", NULL);
+        exit(0);
     }
     if (pid > 0)
     {
-        kill(pid, SIGSTOP);
-        sprintf(ptr, "%s", "Another");
-        ptr += strlen("Another");
-        kill(pid, SIGCONT);
-        cout << "parent wait" << endl;
-        wait(NULL);
-        printf("%s", (char *)ptr);
-        shm_unlink(name);
-        cout << "\nparent done" << endl;
+        for (int i = 0; i < numCPU; i++)
+        {
+            wait(NULL);
+        }
+        cout << "parent run" << endl;
+        cout << getpid() << endl;
+        munmap(ids_shm, ids_shm_size);
+        shm_unlink(ids_shm_name);
+        return 0;
     }
-
-    return 0;
 }
--- Project_1/child.cpp
+++ Project_1/child.cpp
@@ -1,27 +1,33 @@
-#include <sys/wait.h>
 #include <iostream>
 #include <unistd.h>
-#include <sys/mman.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <string.h>
+#include <sys/mman.h> /* for shm_open */
+#include <sys/stat.h> /* For mode constants */
+#include <fcntl.h>    /* For O_* constants */
+#include <signal.h>
+#include <sys/wait.h>
 
 using namespace std;
 
 int main()
 {
-    cout << "child running" << endl;
-    const int SIZE = 4096;
-    const char *name = "OS";
+    int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
+    const char *ids_shm_name = "ids_shm";
+    const int ids_shm_size = sizeof(pid_t);
 
     int fd;
-    char *ptr;
-    fd = shm_open(name, O_RDWR, 0666);
-    ptr = (char *)mmap(0, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-    char *msg = (char *)ptr;
-    ptr+=strlen(msg);
-    cout << msg << endl;
-    sprintf(ptr, "%s", "its child");
-    cout << "\nchild done" << endl;
+    fd = shm_open(ids_shm_name, O_RDWR, 0666);
+    pid_t *ids_shm = (pid_t *)mmap(0, ids_shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    cout << "child run" << endl;
+    for (int i = 0; i < numCPU + 1; i++)
+    {
+        pid_t val = *ids_shm;
+        if (val == getpid())
+        {
+            cout << "I'm " << val << " child " << i << endl;
+            break;
+        }
+        ids_shm += sizeof(val);
+    }
     return 0;
 }
