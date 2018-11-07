a4dcac8655a09573ddcb91f751d776976fbb703c
Sadegh Hmp <sad3gh.hmp@gmail.com>
2018-11-07 15:29:44 +0330

    add Create and Open Memory funcs. add data file.

--- Project_1/Knapsack.cpp
+++ Project_1/Knapsack.cpp
@@ -8,26 +8,21 @@
 
 using namespace std;
 
+void *createMemory(const char *, int);
+void *openMemory(const char *, int);
+
 int main()
 {
-    int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
+    const int numCPU = (int)sysconf(_SC_NPROCESSORS_ONLN);
 
     const char *ids_shm_name = "ids_shm";
     const int ids_shm_size = sizeof(pid_t) * numCPU;
     const char *seeds_shm_name = "seeds_shm";
     const int seeds_shm_size = sizeof(int) * numCPU;
 
-    int fd;
-    fd = shm_open(ids_shm_name, O_CREAT | O_RDWR, 0666);
-    ftruncate(fd, ids_shm_size);
+    pid_t *ids_shm = (pid_t *)createMemory(ids_shm_name, ids_shm_size);
 
-    pid_t *ids_shm = (pid_t *)mmap(0, ids_shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-    fd = shm_open(seeds_shm_name, O_CREAT | O_RDWR, 0666);
-
-    ftruncate(fd, seeds_shm_size);
-
-    int *seeds_shm = (int *)mmap(0, seeds_shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    int *seeds_shm = (int *)createMemory(seeds_shm_name, seeds_shm_size);
 
     srand(time(0));
     pid_t pid;
@@ -60,4 +55,25 @@ int main()
         shm_unlink(ids_shm_name);
         return 0;
     }
+}
+
+void *createMemory(const char *name, int size)
+{
+    int fd;
+    fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+    ftruncate(fd, size);
+
+    void *ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    return ptr;
+}
+
+void *openMemory(const char *name, int size)
+{
+    int fd;
+    fd = shm_open(name, O_RDWR, 0666);
+
+    void *ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    return ptr;
 }
--- Project_1/child.cpp
+++ Project_1/child.cpp
@@ -10,24 +10,21 @@ using namespace std;
 
 int getIndex(pid_t *, int);
 int getSeed(int *, int);
+void *createMemory(const char *, int);
+void *openMemory(const char *, int);
 
 int main()
 {
-    int numCPU = sysconf(_SC_NPROCESSORS_ONLN);
+    const int numCPU = (int)sysconf(_SC_NPROCESSORS_ONLN);
 
     const char *ids_shm_name = "ids_shm";
     const int ids_shm_size = sizeof(pid_t) * numCPU;
     const char *seeds_shm_name = "seeds_shm";
     const int seeds_shm_size = sizeof(int) * numCPU;
 
-    int fd;
-    fd = shm_open(ids_shm_name, O_RDWR, 0666);
+    pid_t *ids_shm = (pid_t *)openMemory(ids_shm_name, ids_shm_size);
 
-    pid_t *ids_shm = (pid_t *)mmap(0, ids_shm_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-    fd = shm_open(seeds_shm_name, O_RDONLY, 0666);
-
-    int *seeds_shm = (int *)mmap(0, seeds_shm_size, PROT_READ, MAP_SHARED, fd, 0);
+    int *seeds_shm = (int *)openMemory(seeds_shm_name, seeds_shm_size);
 
     //cout << "child run" << endl;
     int myIndex = getIndex(ids_shm, numCPU);
@@ -53,4 +50,25 @@ int getIndex(pid_t *ptr, int num)
 int getSeed(int *ptr, int index)
 {
     return ptr[index];
+}
+
+void *createMemory(const char *name, int size)
+{
+    int fd;
+    fd = shm_open(name, O_CREAT | O_RDWR, 0666);
+    ftruncate(fd, size);
+
+    void *ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    return ptr;
+}
+
+void *openMemory(const char *name, int size)
+{
+    int fd;
+    fd = shm_open(name, O_RDWR, 0666);
+
+    void *ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+
+    return ptr;
 }
