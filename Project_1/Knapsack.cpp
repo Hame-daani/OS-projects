f8611632c13be196791aed4338df29c8ee642e8f
Sadegh Hmp <sad3gh.hmp@gmail.com>
2018-11-12 13:25:18 +0330

    done.

--- Project_1/Knapsack.cpp
+++ Project_1/Knapsack.cpp
@@ -3,29 +3,52 @@
 #include <sys/mman.h> /* for shm_open */
 #include <sys/stat.h> /* For mode constants */
 #include <fcntl.h>    /* For O_* constants */
-#include <signal.h>
 #include <sys/wait.h>
 #include <fstream>
 
 using namespace std;
 
 void *createMemory(const char *, int);
-void *openMemory(const char *, int);
+
+#define IDEAL_WEIGHT 0
+#define TIME 1
 
 int main()
 {
     const int numCPU = (int)sysconf(_SC_NPROCESSORS_ONLN);
 
     const char *ids_shm_name = "ids_shm";
-    const int ids_shm_size = sizeof(pid_t) * numCPU;
     const char *seeds_shm_name = "seeds_shm";
+    const char *result_shm_name = "result_shm";
+    const char *params_shm_name = "params_shm";
+    const char *result_value_shm_name = "result_value_shm";
+
+    const int params_shm_size = sizeof(int) * 2;
+    const int ids_shm_size = sizeof(pid_t) * numCPU;
     const int seeds_shm_size = sizeof(int) * numCPU;
+    const int result_value_shm_size = sizeof(int) * numCPU;
+    const int result_shm_size = sizeof(int) * numCPU * 100;
 
     pid_t *ids_shm = (pid_t *)createMemory(ids_shm_name, ids_shm_size);
 
     int *seeds_shm = (int *)createMemory(seeds_shm_name, seeds_shm_size);
 
-    srand(time(0));
+    int *params_shm = (int *)createMemory(params_shm_name, params_shm_size);
+
+    int *result_shm = (int *)createMemory(result_shm_name, result_shm_size);
+
+    int *result_value_shm = (int *)createMemory(result_value_shm_name, result_value_shm_size);
+
+    int a;
+    cout << "Insert Ideal Weight: ";
+    cin >> a;
+    params_shm[IDEAL_WEIGHT] = a;
+    cout << "Insert Run Time: ";
+    cin >> a;
+    params_shm[TIME] = a;
+
+    srand(time(NULL));
+
     pid_t pid = 0;
     for (int i = 0; i < numCPU; i++)
     {
@@ -48,17 +71,44 @@ int main()
         {
             wait(NULL);
         }
+
         cout << "parent run" << endl;
-        for (int i = 0; i < numCPU; i++)
+
+        for (int j = 0; j < numCPU; j++)
         {
-            cout << "child " << i << " id: " << ids_shm[i] << " seed: " << seeds_shm[i] << endl;
+            cout << "child " << j + 1 << " - "
+                 << "Value : " << result_value_shm[j] << endl;
+
+            for (int i = 0; i < 100; i++)
+            {
+                if (result_shm[(j * 100) + i] == 0)
+                {
+                    cout << endl
+                         << endl;
+                    break;
+                }
+
+                cout << result_shm[(j * 100) + i] << " ";
+                if (i > 0 && i % 10 == 0)
+                    cout << endl;
+            }
         }
-        cout << getpid() << endl;
+
         munmap(ids_shm, ids_shm_size);
         shm_unlink(ids_shm_name);
 
         munmap(seeds_shm, seeds_shm_size);
         shm_unlink(seeds_shm_name);
+
+        munmap(result_shm, result_shm_size);
+        shm_unlink(result_shm_name);
+
+        munmap(params_shm, params_shm_size);
+        shm_unlink(params_shm_name);
+
+        munmap(result_value_shm, result_value_shm_size);
+        shm_unlink(result_value_shm_name);
+
         return 0;
     }
 }
@@ -68,16 +118,6 @@ void *createMemory(const char *name, int size)
     int fd;
     fd = shm_open(name, O_CREAT | O_RDWR, 0666);
     ftruncate(fd, size);
-
-    void *ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-    return ptr;
-}
-
-void *openMemory(const char *name, int size)
-{
-    int fd;
-    fd = shm_open(name, O_RDWR, 0666);
 
     void *ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
 
--- Project_1/child.cpp
+++ Project_1/child.cpp
@@ -3,37 +3,51 @@
 #include <sys/mman.h> /* for shm_open */
 #include <sys/stat.h> /* For mode constants */
 #include <fcntl.h>    /* For O_* constants */
-#include <signal.h>
-#include <sys/wait.h>
 #include <fstream>
 #include <string.h>
+#include <vector>
 
 using namespace std;
 
 int getIndex(pid_t *, int);
 int getSeed(int *, int);
-void *createMemory(const char *, int);
 void *openMemory(const char *, int);
-bool repeatCheck(int *, int, int);
-int getWeightSum(int *, int, int[100][2]);
-bool exceedWeight(int *, int, int, int[100][2], int);
+bool repeatCheck(vector<int> &, int);
+int getWeightSum(vector<int> &, int[100][2]);
+int getValueSum(vector<int> &, int[100][2]);
+bool exceedWeight(vector<int> &, int[100][2], int);
+void process(vector<int> &, int[100][2], int);
 
 #define WEIGHT 0
 #define VALUE 1
-#define IDEAL_WEIGHT 1000
+#define IDEAL_WEIGHT 0
+#define TIME 1
 
 int main()
 {
     const int numCPU = (int)sysconf(_SC_NPROCESSORS_ONLN);
 
     const char *ids_shm_name = "ids_shm";
-    const int ids_shm_size = sizeof(pid_t) * numCPU;
     const char *seeds_shm_name = "seeds_shm";
+    const char *result_shm_name = "result_shm";
+    const char *params_shm_name = "params_shm";
+    const char *result_value_shm_name = "result_value_shm";
+
+    const int params_shm_size = sizeof(int) * 2;
+    const int ids_shm_size = sizeof(pid_t) * numCPU;
     const int seeds_shm_size = sizeof(int) * numCPU;
+    const int result_value_shm_size = sizeof(int) * numCPU;
+    const int result_shm_size = sizeof(int) * numCPU * 100;
 
     pid_t *ids_shm = (pid_t *)openMemory(ids_shm_name, ids_shm_size);
 
     int *seeds_shm = (int *)openMemory(seeds_shm_name, seeds_shm_size);
+
+    int *params_shm = (int *)openMemory(params_shm_name, params_shm_size);
+
+    int *result_shm = (int *)openMemory(result_shm_name, result_shm_size);
+
+    int *result_value_shm = (int *)openMemory(result_value_shm_name, result_value_shm_size);
 
     int data[100][2];
     ifstream f;
@@ -49,29 +63,28 @@ int main()
     int mySeed = getSeed(seeds_shm, myIndex);
     srand(mySeed);
 
-    if (myIndex == 0)
-    {
-        int num = rand() % 100 + 1;
-        int *result = new int[num];
-        int n;
-        memset(result, 0, num * sizeof(*result));
-        for (int j = 0; j < num; j++)
-        {
-            n = rand() % 100 + 1;
-            while (repeatCheck(result, num, n) || exceedWeight(result, num, n, data, IDEAL_WEIGHT))
-                n = rand() % 100 + 1;
-            cout << n << endl;
-            result[j] = n;
-        }
+    vector<int> bestResult;
 
-        for (int j = 0; j < num; j++)
-        {
-            cout << j << " : " << result[j] << " - " << data[result[j]][WEIGHT] << endl;
-            ;
-        }
-        cout << getWeightSum(result, num, data);
-        delete result;
+    cout << "Child " << myIndex + 1 << " Running..." << endl;
+
+    int runTime = params_shm[TIME];
+
+    time_t start = time(NULL);
+    time_t now = time(NULL);
+    
+    while ((now - start) <= runTime)
+    {
+        process(bestResult, data, params_shm[IDEAL_WEIGHT]);
+        now = time(NULL);
     }
+
+    result_value_shm[myIndex] = getValueSum(bestResult, data);
+
+    for (int j = 0; j < bestResult.size(); j++)
+    {
+        result_shm[(myIndex * 100) + j] = bestResult[j];
+    }
+
     return 0;
 }
 
@@ -91,17 +104,6 @@ int getSeed(int *ptr, int index)
     return ptr[index];
 }
 
-void *createMemory(const char *name, int size)
-{
-    int fd;
-    fd = shm_open(name, O_CREAT | O_RDWR, 0666);
-    ftruncate(fd, size);
-
-    void *ptr = mmap(0, size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
-
-    return ptr;
-}
-
 void *openMemory(const char *name, int size)
 {
     int fd;
@@ -112,31 +114,60 @@ void *openMemory(const char *name, int size)
     return ptr;
 }
 
-bool repeatCheck(int *result, int num, int n)
+bool repeatCheck(vector<int> &vec, int n)
 {
-    cout << "repeatCheck" << endl;
-    for (int i = 0; i < num; i++)
+    for (int i = 0; i < vec.size(); i++)
     {
-        if (result[i] == n)
+        if (vec[i] == n)
             return true;
     }
     return false;
 }
 
-int getWeightSum(int *result, int num, int data[100][2])
+int getWeightSum(vector<int> &vect, int data[100][2])
 {
-    cout << "sum" << endl;
     int sum = 0;
-    for (int i = 0; i < num; i++)
+    for (int i = 0; i < vect.size(); i++)
     {
-        sum += data[result[i]][WEIGHT];
+        sum += data[vect[i]][WEIGHT];
     }
     return sum;
 }
 
-bool exceedWeight(int *result, int num, int n, int data[100][2], int ideal)
+int getValueSum(vector<int> &vect, int data[100][2])
 {
-    cout << "exceed" << endl;
-    int sum = getWeightSum(result, num, data);
-    return ((sum + data[n][WEIGHT]) > ideal);
+    int sum = 0;
+    for (int i = 0; i < vect.size(); i++)
+    {
+        sum += data[vect[i]][VALUE];
+    }
+    return sum;
+}
+
+bool exceedWeight(vector<int> &vect, int data[100][2], int ideal)
+{
+    int sum = getWeightSum(vect, data);
+    return sum > ideal;
+}
+
+void process(vector<int> &bestResult, int data[100][2], int ideal)
+{
+    int n;
+    vector<int> result;
+    do
+    {
+        do
+        {
+            n = rand() % 100 + 1;
+        } while (repeatCheck(result, n));
+
+        result.push_back(n);
+
+    } while (!exceedWeight(result, data, ideal));
+
+    result.pop_back();
+    if (getValueSum(result, data) > getValueSum(bestResult, data))
+    {
+        bestResult = result;
+    }
 }
